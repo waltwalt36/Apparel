e34aca332f344c9e977df02c72d5072191966fdc
Walter <wbrione@clemson.edu>
2026-03-30 13:47:11 -0400

    Added simple text to login and signup screens, changed a couple files to jsx, and wired up app to include signup and login components

--- mobile/App.jsx
+++ mobile/App.jsx
@@ -1,5 +1,7 @@
 import { View, Text } from 'react-native';
 import { Amplify } from 'aws-amplify';
+import { Login } from './src/screens/LoginScreen.jsx';
+import { SignUp } from './src/screens/SignUpScreen.jsx';
 const API_BASE_URL = process.env.EXPO_PUBLIC_API_URL;
 
 Amplify.configure({
@@ -21,8 +23,10 @@ Amplify.configure({
 
 export default function App() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Hello World</Text>
-    </View>
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Apparel</Text>
+        <Text><Login /></Text>
+        <Text><SignUp /></Text>
+      </View>
   );
 }
---
+++ mobile/src/screens/LoginScreen.jsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export const Login = () => {
+    return (
+        <View>
+            <Text for="email">Login</Text>
+        </View>
+    )
+}
